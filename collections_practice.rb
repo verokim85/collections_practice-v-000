b9f82ec4e27015b731f9a8cb6653bf624147d2a7
Veronica Kim <vero.a.kim@gmail.com>
2018-01-29 02:24:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
+def sort_array_asc(array)
+  array.sort
+end
+
 def sort_array_desc(array)
   array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b| a.length <=>b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b| a[1]<=>b[2]
+  end
+end
+
+def reverse_array(array)
+  x = array.reverse
+end
+
+
+def kesha_maker(array)
+ x = array.each do |kesha|
+    kesha[2] = "$"
+  end
+  x
+end
+
+def find_a(array)
+  array.select { |i| i[0] == "a" }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n}
+end
+
+def add_s(array)
+
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element = element
+    else
+      element + "s"
+  end
+end
 end
